f5394223df482212128ebb83cd0c0aff23d2fe13
doannd <nguyendinhdoanjdk@gmail.com>
2019-02-12 21:51:11 +0700

    [toeicofme] Building fillAll method

--- src/main/java/com/nguyendinhdoan/toeic/dao/GenericDaoImpl.java
+++ src/main/java/com/nguyendinhdoan/toeic/dao/GenericDaoImpl.java
@@ -1,18 +1,46 @@
 package com.nguyendinhdoan.toeic.dao;
 
 import com.nguyendinhdoan.toeic.common.HibernateUtils;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenericDaoImpl<Id extends Serializable, T> implements GenericDao<Id, T> {
+
+    private Class<T> persistenceClass;
+
+    public GenericDaoImpl() {
+        this.persistenceClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+    }
+
+    public String getPersistenceClassName() {
+        return persistenceClass.getSimpleName();
+    }
 
     public Session getSession() {
         return HibernateUtils.getSessionFactory().openSession();
     }
 
     public List<T> findAll() {
-        return null;
+        List<T> listData = new ArrayList<T>();
+        Transaction transaction = null;
+        try {
+            transaction = getSession().beginTransaction();
+            StringBuilder sql = new StringBuilder("from ");
+            sql.append(getPersistenceClassName());
+            Query query = getSession().createQuery(sql.toString());
+            listData = query.list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            throw e;
+        }
+        return listData;
     }
 }
